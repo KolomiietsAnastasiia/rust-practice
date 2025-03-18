1f9811fa334c62d7f9b31acc453809ec12798021
AnastasiiaKolomiiets <anastasiia.kolomiiets@e-u.edu.ua>
2025-03-18 18:38:30 +0200

    Конверт (Практична робота №3) виправлення

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,14 +1,16 @@
 #[test]
 fn envelope() {
-    const W: u32 = 25;
-    const H: u32 = 10;
+    const W: i32 = 25;
+    const H: i32 = 10;
 
     for y in 0..H {
+        let step = y * W/(H - 1);
+
         for x in 0..W {
             let is_hor = y == 0 || y == H - 1;
             let is_ver = x == 0 || x == W - 1;
-            let is_diag = x == y * W/H;
-            let is_diag2 = x == W - 1 - y * W/H;
+            let is_diag = x == step;
+            let is_diag2 = x == W - 1 - step;
             let to_show = is_hor || is_ver || is_diag || is_diag2;
 
             let sym= if to_show { '*' } else { ' ' };
@@ -19,14 +21,15 @@ fn envelope() {
     }
 }
 
-//output:
-//  *************************
-//  * *                   * *
-//  *    *             *    *
-//  *      *         *      *
-//  *         *   *         *
-//  *           *           *
-//  *        *     *        *
-//  *      *         *      *
-//  *   *               *   *
-//  *************************
+//OUTPUT
+// W = 25; H = 10;                  W = 10; H = 10;
+// *************************        **********
+// * *                   * *        **      **
+// *    *             *    *        * *    * *
+// *       *       *       *        *  *  *  *
+// *          * *          *        *   **   *
+// *          * *          *        *   **   *
+// *       *       *       *        *  *  *  *
+// *    *             *    *        * *    * *
+// * *                   * *        **      **
+// *************************        **********
